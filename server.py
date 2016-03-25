bbc2003329880018c2e585deb245a9a486ada99c
pahaz <pahaz.blinov@gmail.com>
2016-03-25 17:29:30 +0500

    add server v0

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+from wsgiref.validate import validator
 from wsgiref.simple_server import make_server
 from app import application
 
-httpd = make_server('', 8001, application)
+httpd = make_server('', 8001, validator(application))
 print("Listening on port 8001....")
 httpd.serve_forever()
