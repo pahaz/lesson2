f43775bbb5ecdb0b95d3016ecccad119ff7ea6e3
pahaz <pahaz.blinov@gmail.com>
2016-03-28 16:16:55 +0500

    fix(local.py): set global variable bug

--- gitdata/local.py
+++ gitdata/local.py
@@ -5,7 +5,4 @@ def load(names):
     for name in names:
         with open(name + '.json') as f:
             data = f.read().strip()
-            setattr(
-                globals(),
-                name,
-                json.loads(data))
+            globals()[name] = json.loads(data)
